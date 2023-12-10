bb14ea53c1aabac2a84443419633ed3ea38c682e
Eduardo Rezzadori <r.eduardo.23@outlook.com>
2023-12-09 23:39:58 -0300

    arrumando a casa

--- organograma/src/App.js
+++ organograma/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
 import Banner from './components/Banner/Banner'
 
 function App() {
   return (
-    <Banner />
+    <div className="App">
+      <Banner />
+    </div>
   );
 }
 
